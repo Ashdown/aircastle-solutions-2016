e087293e94df7f3aced7e12af2735da97580f128
Rory Devane <rory.devane@gmail.com>
2017-01-13 15:57:06 +0000

    styled up header elements

--- assets/javascripts/app/components/Header.react.js
+++ assets/javascripts/app/components/Header.react.js
@@ -2,14 +2,19 @@
 
     App.Components.Header = React.createClass({
 
-        render: function(){
+        render: function () {
 
             return (
                 <header className="header">
                     <h1 className="page-title">Rory Devane</h1>
-                    <p className="intro-para">Full Stack Web Developer</p>
+                    <p className="description-para">Full Stack Web Developer</p>
+                    <p className="intro-para">
+                    Hi, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                    nisi ut aliquip ex ea commodo consequat.
+                    </p>
                 </header>
-            )
+                )
         }
 
     });
