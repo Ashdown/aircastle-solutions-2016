2b99a7f40b5b6f12f0eba22b2934816594022ab5
Rory Devane <rory.devane@gmail.com>
2017-01-16 16:32:20 +0000

    styled mobile version of about module

--- assets/javascripts/app/components/About.react.js
+++ assets/javascripts/app/components/About.react.js
@@ -7,12 +7,13 @@
 
             return(
                 <div className="about">
-                    <p>{aboutData.description}</p>
                     <img className="about-image"
-                        src={aboutData.image.src}
-                        alt={aboutData.image.alt}
-                        height="120"
-                        width="120" />
+                    src={aboutData.image.src}
+                    alt={aboutData.image.alt}
+                    height="120"
+                    width="120" />
+                    <p className="description">{aboutData.description}</p>
+
                 </div>
                 );
         }
