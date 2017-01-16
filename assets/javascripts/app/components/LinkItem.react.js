7af8ba0f747ee268c4c7c33b7785e1353b489a71
Rory Devane <rory.devane@gmail.com>
2017-01-16 17:30:20 +0000

    styled links module for mobile

--- assets/javascripts/app/components/LinkItem.react.js
+++ assets/javascripts/app/components/LinkItem.react.js
@@ -8,10 +8,26 @@
 
         render: function() {
 
-            var data = this.props.data;
+            var data = this.props.data,
+                logo;
+
+            console.log(data.type);
+
+            switch(data.type) {
+                case 'linked-in':
+                    logo = <App.Components.LinkedInSvg/>;
+                    break;
+                case 'github':
+                    logo = <App.Components.GitHubSvg/>;
+                    break;
+            }
+
             return(
                 <li>
-                    <a href={data.destination}>{data.text}</a>
+                    <a className="link" href={data.destination}>
+                        {logo}
+                        {data.text}
+                    </a>
                 </li>);
         }
     })
