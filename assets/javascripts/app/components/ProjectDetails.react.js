b996ef4b8e2a40a13c8fdc2a56f2b02306cf0605
Rory Devane <rory.devane@gmail.com>
2017-01-18 14:29:13 +0000

    desktop version of big overlay

--- assets/javascripts/app/components/ProjectDetails.react.js
+++ assets/javascripts/app/components/ProjectDetails.react.js
@@ -13,18 +13,20 @@
 
             return(
                 <div className={"project-details " + this.props.toggleClass + ' ' + this.props.columnClass}>
-                    <h2 className="project-title">{data.title}</h2>
-                    <a className="close-link" href="#" onClick={this.props.toggleFunction}>
-                        <App.Components.CloseSvg />
-                    </a>
-                    <App.Components.KeywordContainer data={data} />
-                    <img className="details-image" src={data.image.src} alt={data.image.alt} height={data.image.height} width={data.image.width} />
-                    <p className="dates">
-                        <span className="start-date">{data.start}</span>
-                        <span className="end-date"> → {data.end}</span>
-                    </p>
-                    <p className="description sub-item">{data.description}</p>
-                    <ul className="project-link-list">{links}</ul>
+                    <div className="details-container">
+                        <h2 className="project-title">{data.title}</h2>
+                        <a className="close-link" href="#" onClick={this.props.toggleFunction}>
+                            <App.Components.CloseSvg />
+                        </a>
+                        <App.Components.KeywordContainer data={data} />
+                        <img className="details-image" src={data.image.src} alt={data.image.alt} height={data.image.height} width={data.image.width} />
+                        <p className="dates">
+                            <span className="start-date">{data.start}</span>
+                            <span className="end-date"> → {data.end}</span>
+                        </p>
+                        <p className="description sub-item">{data.description}</p>
+                        <ul className="project-link-list">{links}</ul>
+                    </div>
 
                 </div>
                 );
