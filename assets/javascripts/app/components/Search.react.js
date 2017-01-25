2f03e71edbe380fa39566ee1d136adafe4cac233
Rory Devane <rory.devane@gmail.com>
2017-01-25 11:33:47 +0000

    styled mobile version of search component

--- assets/javascripts/app/components/Search.react.js
+++ assets/javascripts/app/components/Search.react.js
@@ -1,13 +1,45 @@
 (function(App, React) {
 
     App.Components.Search = React.createClass({
+
+        mouseEnter: function() {
+            this.setState({
+                hoverClass: 'hover'
+            });
+        },
+
+        mouseLeave: function() {
+            this.setState({
+                hoverClass: ''
+            });
+        },
+
+        inputEnter: function() {
+            this.setState({
+                inputStatus: 'active'
+            });
+        },
+
+        inputLeave: function() {
+            this.setState({
+                inputStatus: 'not-active'
+            })
+        },
+
+        getInitialState: function() {
+            return ({
+                hoverClass: '',
+                inputStatus: 'not-active'
+            });
+        },
+
         render: function() {
             return(
-                <div className="search">
-                    <h3>Search</h3>
-                    <form action="#">
-                        <input className="search-input" type="text" />
-                        <input type="submit" value="search" />
+                <div className={"search " + this.state.hoverClass} onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+                    <h3 className="search-title" >Search</h3>
+                    <form className="search-form" action="#">
+                        <input className={"search-input " + this.state.inputStatus} onFocus={this.inputEnter} onBlur={this.inputLeave} type="text" />
+                        <input className="search-button" type="submit" value="search" />
                     </form>
                 </div>
             );
