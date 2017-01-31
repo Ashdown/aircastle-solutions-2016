17b86119e7e4a5df49cd7f521f8f9552934fa9c1
Rory Devane <rory.devane@gmail.com>
2017-01-31 12:45:45 +0000

    adding back up images from cms

--- assets/javascripts/app/stores/ProjectStore.js
+++ assets/javascripts/app/stores/ProjectStore.js
@@ -1,4 +1,4 @@
-(function(React, App, assign){
+(function (React, App, assign) {
 
     var CHANGE_EVENT_STRING = 'project_change';
     var CHANGE_EVENT = new Event(CHANGE_EVENT_STRING);
@@ -12,12 +12,24 @@
     function create(data) {
         //use timestamp as the id
         var id = Date.now();
+
+        var images = [];
+
+        for (var i in data.pictures) {
+            images.push({
+                height: data.pictures[i].height,
+                width: data.pictures[i].width,
+                alt: data.pictures[i].alt,
+                url: data.pictures[i].url.replace('http://cms.rorydevane.com/wp-content/', '/assets/')
+            });
+        }
+
         _projects.push({
             id: id,
             title: data.title,
             type: data.type,
             description: data.description,
-            images: data.pictures,
+            images: images,
             links: data.links,
             keywords: data.keywords,
             start: new Date(data.startDate),
@@ -26,27 +38,28 @@
 
         });
         //sort by date
-        _projects.sort(function(a, b) {
+        _projects.sort(function (a, b) {
             return b.start.getTime() - a.start.getTime();
         })
     }
 
-    App.Stores.ProjectStore = function(){};
+    App.Stores.ProjectStore = function () {
+    };
 
     App.Stores.ProjectStore = assign({}, App.Stores.ProjectStore.prototype, {
 
-        getAll: function() {
+        getAll: function () {
             return _projects;
         },
 
-        filterByKeyword: function(keyword) {
+        filterByKeyword: function (keyword) {
             console.log('filter');
-            for(var i in _projects) {
+            for (var i in _projects) {
                 _projects[i].matchesFilter = false;
 
-                for(var j in _projects[i].keywords) {
+                for (var j in _projects[i].keywords) {
 
-                    if(_projects[i].keywords[j].indexOf(keyword) !== -1) {
+                    if (_projects[i].keywords[j].indexOf(keyword) !== -1) {
                         _projects[i].matchesFilter = true;
                         break;
                     }
@@ -55,19 +68,19 @@
         },
 
 
-        addChangeListener: function(callback) {
+        addChangeListener: function (callback) {
             App.target.addEventListener(CHANGE_EVENT_STRING, callback);
         },
 
-        removeChangeListener: function(callback) {
+        removeChangeListener: function (callback) {
             App.target.removeEventListener(CHANGE_EVENT_STRING, callback);
         },
 
-        emitChange: function() {
+        emitChange: function () {
             App.target.dispatchEvent(CHANGE_EVENT)
         },
 
-        dispatcherIndex: App.Dispatcher.register(function(payload) {
+        dispatcherIndex: App.Dispatcher.register(function (payload) {
             var action = payload.action;
             var data;
 
