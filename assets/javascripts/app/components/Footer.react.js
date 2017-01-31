40d785783009492bf5bb894dd3e899d99fe19b9a
Rory Devane <rory.devane@gmail.com>
2017-01-31 14:03:18 +0000

    copy updates

--- assets/javascripts/app/components/Footer.react.js
+++ assets/javascripts/app/components/Footer.react.js
@@ -3,7 +3,7 @@
     App.Components.Footer = React.createClass({
         render: function() {
             return(
-                <footer className="footer">Website design &copy; Rory Devane 2017</footer>
+                <footer className="footer">Website &copy; Rory Devane 2017</footer>
                 );
         }
     });
--- assets/javascripts/app/components/Header.react.js
+++ assets/javascripts/app/components/Header.react.js
@@ -27,7 +27,7 @@
             return (
                 <header className="header">
                     <h1 className="page-title">Rory Devane</h1>
-                    <p className="description-para">Full Stack Web Developer</p>
+                    <p className="description-para">Web Developer</p>
                     <p className="intro-para">{this.state.headerData.introduction}</p>
                     <App.Components.Navigation />
                 </header>
